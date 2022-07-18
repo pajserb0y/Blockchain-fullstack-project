50812c0037587009b74fd81c30b5dbb7c0853aa2
pajserb0y <65039799+pajserb0y@users.noreply.github.com>
2022-07-18 14:30:14 +0200

    gas-report for tested functions

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -2,12 +2,15 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -24,5 +27,13 @@ module.exports = {
     solidity: "0.8.9",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        //token: "MATIC", //poligon blockchain
     },
 }
